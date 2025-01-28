c92fc44efcd0f3aae45347074a712cd5c0ea6c39
Viswajith2003 <viswajithviswa715@gmail.com>
2025-01-28 17:36:36 +0530

    update workgin

--- src/UI/todo.jsx
+++ src/UI/todo.jsx
@@ -1,35 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { MdDeleteOutline } from "react-icons/md";
 import { FaRegEdit } from "react-icons/fa";
-import { useState } from "react";
 
 export default function Todo() {
   const [input, setInput] = useState("");
   const [todos, setTodos] = useState([]);
+  const [edit, setEdit] = useState({ id: null, list: "" });
 
-  const addTodo = () => {
-    setTodos([...todos,{list:input,id:Date.now()}]);
-    setInput('')
-    console.log(todos);
+  const handleAddOrUpdate = () => {
+    if (edit.id) {
+      setTodos(todos.map(data => data.id === edit.id ? { ...data, list: input } : data));
+      setEdit({ id: null, list: "" });
+    } else {
+      setTodos([...todos, { list: input, id: Date.now() }]);
+    }
+    setInput('');
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    handleAddOrUpdate();
   };
 
-  const setDelete=(id)=>{
-    setTodos(todos.filter((data)=>data.id !== id))
-  }
+  const setDelete = (id) => {
+    setTodos(todos.filter((data) => data.id !== id));
+  };
 
+  const setEditTodo = (id, list) => {
+    setEdit({ id, list });
+    setInput(list);
+  };
 
   return (
     <div className="flex justify-center items-center text-center ">
       <div
-        className="bg-[#10173c] text-white rounded-lg  overflow-hidden shadow-lg items-center justify-center text-center mt-20"
+        className="bg-[#10173c] text-white rounded-lg overflow-hidden shadow-lg items-center justify-center text-center mt-20"
         style={{ width: "550px" }}
       >
         <h1 className="font-bold text-4xl mt-6 mb-6">TO DO APP</h1>
-        <form action="submit" onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit}>
           <input
             type="text"
             value={input}
@@ -38,26 +47,25 @@ export default function Todo() {
             onChange={(e) => setInput(e.target.value)}
           />
           <button
-            type="button"
+            type="submit"
             className="ml-4 border-2 border-white w-20 h-12 font-bold rounded hover:bg-[#2a61a9] hover:text-lg"
-            onClick={addTodo}
           >
-            ADD
+            {edit.id ? "UPDATE" : "ADD"}
           </button>
         </form>
         <div className="flex justify-center items-center mb-12">
           <ul>
-            {todos.map((data)=>(
-              <li className="bg-[#1e4a83] w-96 mt-8 text-left p-2 rounded flex justify-between">
-              <div className="flex gap-2">
-                <input type="checkbox" className="w-5" />
-                <p className="text-lg">{data.list}</p>
-              </div>
-              <div className="flex gap-2 items-center">
-                <FaRegEdit className="w-6 h-6" />
-                <MdDeleteOutline className="w-6 h-6" onClick={()=>setDelete(data.id)}/>
-              </div>
-            </li>
+            {todos.map((data) => (
+              <li key={data.id} className="bg-[#1e4a83] w-96 mt-8 text-left p-2 rounded flex justify-between">
+                <div className="flex gap-2">
+                  <input type="checkbox" className="w-5" />
+                  <p className="text-lg">{data.list}</p>
+                </div>
+                <div className="flex gap-2 items-center">
+                  <FaRegEdit className="w-6 h-6" onClick={() => setEditTodo(data.id, data.list)} />
+                  <MdDeleteOutline className="w-6 h-6" onClick={() => setDelete(data.id)} />
+                </div>
+              </li>
             ))}
           </ul>
         </div>
@@ -65,3 +73,4 @@ export default function Todo() {
     </div>
   );
 }
+
